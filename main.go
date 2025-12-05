868dca546e153e890bc5b382ba51e7bd6f53c0bf
Poype <253186926@qq.com>
2025-12-05 21:53:12 +0800

    package & import

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+/*
+ 1. 每个文件必须要以 package 开头
+ 2. math/rand 是import path， rand 是 package
+*/
 
 func main() {
 	fmt.Println("Hello 世界！")
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(math.Abs(-100))
 }
 
 // 每条语句后面无需 ;
