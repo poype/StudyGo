0b960d83a3fc198d8fa5823712d96b50c56208f4
Poype <253186926@qq.com>
2025-12-07 10:51:40 +0800

    指针

---
+++ pointer/pointer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i = 100
+	// *int 类型，跟C语言比是反着的，这里的 *int 无需明确指定
+	var p *int = &i
+	*p++
+	fmt.Println(*p)
+
+	a, b := 1, 2
+	swap(&a, &b)
+	fmt.Println(a, b)
+}
+
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+
+// Unlike C, Go has no pointer arithmetic.
